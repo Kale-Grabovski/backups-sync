a0677e9e96c56a0a1cc4911799f5c52ab1f8acdc
ant <ant@ant>
2023-05-04 12:14:33 +0300

    fuck you

--- uploader.sh
+++ uploader.sh
@@ -3,5 +3,5 @@ FILES="/var/lib/postgresql/backups/*.bz2"
 for f in $FILES
 do
   fn="$(basename -- $f)"
-  /var/lib/postgresql/dbxcli put $f $1/`basename -- $fn`.bz2
+  /var/lib/postgresql/dbxcli put $f $1/`basename -- $fn`
 done
